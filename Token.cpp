f1e5367ee102fa119fffefa981c19d8e8a1f0318
lulldev <lulldev@yandex.ru>
2019-01-09 00:13:54 +0300

    fix other code

--- Token.cpp
+++ Token.cpp
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "Token.h"
 #include "Error.h"
 
@@ -12,19 +11,19 @@ const char *Token::toString()
     return fmtstr("%c", tag);
 }
 
-Word *Word::True    = new Word("true",  Tag::TRUE);
-Word *Word::False   = new Word("false", Tag::FALSE);
-Word *Word::And     = new Word("&&",    Tag::AND);
-Word *Word::Or      = new Word("||",    Tag::OR);
-Word *Word::Eq      = new Word("==",    Tag::EQ);
-Word *Word::Ne      = new Word("!=",    Tag::NE);
-Word *Word::Le      = new Word("<=",    Tag::LE);
-Word *Word::Ge      = new Word(">=",    Tag::GE);
-Word *Word::Minus   = new Word("minus", Tag::MINUS);
-Word *Word::Temp    = new Word("t",     Tag::TEMP);
+Word *Word::True = new Word("true", Tag::TRUE);
+Word *Word::False = new Word("false", Tag::FALSE);
+Word *Word::And = new Word("&&", Tag::AND);
+Word *Word::Or = new Word("||", Tag::OR);
+Word *Word::Eq = new Word("==", Tag::EQ);
+Word *Word::Ne = new Word("!=", Tag::NE);
+Word *Word::Le = new Word("<=", Tag::LE);
+Word *Word::Ge = new Word(">=", Tag::GE);
+Word *Word::Minus = new Word("minus", Tag::MINUS);
+Word *Word::Temp = new Word("t", Tag::TEMP);
 
 Word::Word(const char *s, int tag)
-:Token(tag)
+        : Token(tag)
 {
     this->lexme = s;
 }
@@ -34,13 +33,13 @@ const char *Word::toString()
     return lexme;
 }
 
-Type *Type::Int     = new Type("int",       Tag::BASIC, 4);
-Type *Type::Float   = new Type("float",     Tag::BASIC, 8);
-Type *Type::Char    = new Type("char",      Tag::BASIC, 1);
-Type *Type::Bool    = new Type("bool",      Tag::BASIC, 1);
+Type *Type::Int = new Type("int", Tag::BASIC, 4);
+Type *Type::Float = new Type("float", Tag::BASIC, 8);
+Type *Type::Char = new Type("char", Tag::BASIC, 1);
+Type *Type::Bool = new Type("bool", Tag::BASIC, 1);
 
 Type::Type(const char *s, int tag, int w)
-:Word(s, tag)
+        : Word(s, tag)
 {
     this->width = w;
 }
@@ -52,22 +51,23 @@ bool Type::numric(Type *p)
 
 Type *Type::max(Type *p1, Type *p2)
 {
-    if (!numric(p1) || !numric(p2)) {
-        return NULL;
-    }
-    else if (p1 == Type::Float || p2 == Type::Float) {
+    if (!numric(p1) || !numric(p2))
+    {
+        return nullptr;
+    } else if (p1 == Type::Float || p2 == Type::Float)
+    {
         return Type::Float;
-    }
-    else if (p1 == Type::Int || p2 == Type::Int) {
+    } else if (p1 == Type::Int || p2 == Type::Int)
+    {
         return Type::Int;
-    }
-    else {
+    } else
+    {
         return Type::Char;
     }
 }
 
 Array::Array(int sz, Type *p)
-:Type("[]", Tag::INDEX, sz*p->width), size(1)
+        : Type("[]", Tag::INDEX, sz * p->width), size(1)
 {
     this->size = sz;
     this->of = p;
@@ -79,7 +79,7 @@ const char *Array::toString()
 }
 
 Num::Num(int v)
-:Token(Tag::NUM)
+        : Token(Tag::NUM)
 {
     this->value = v;
 }
@@ -90,7 +90,7 @@ const char *Num::toString()
 }
 
 Real::Real(float v)
-:Token(Tag::REAL)
+        : Token(Tag::REAL)
 {
     this->value = v;
 }
--- Token.h
+++ Token.h
@@ -1,6 +1,8 @@
-class Tag {
+class Tag
+{
 public:
-    enum {
+    enum
+    {
         AND = 256,
         BASIC,
         BREAK,
@@ -24,70 +26,83 @@ public:
     };
 };
 
-class Token {
+class Token
+{
 public:
     int tag;
+
     Token(int t);
+
     virtual const char *toString();
 };
 
-class Word : public Token {
+class Word : public Token
+{
 public:
     const char *lexme;
-    
+
     Word(const char *s, int tag);
-    
-    const char *toString();
-    
+
+    const char *toString() override;
+
 public:
-    static Word     *True;
-    static Word     *False;
-    static Word     *And;
-    static Word     *Or;
-    static Word     *Eq;
-    static Word     *Ne;
-    static Word     *Le;
-    static Word     *Ge;
-    static Word     *Minus;
-    static Word     *Temp;
+    static Word *True;
+    static Word *False;
+    static Word *And;
+    static Word *Or;
+    static Word *Eq;
+    static Word *Ne;
+    static Word *Le;
+    static Word *Ge;
+    static Word *Minus;
+    static Word *Temp;
 };
 
-class Type : public Word {
+class Type : public Word
+{
 public:
     int width;
-    
+
 public:
     Type(const char *s, int tag, int w);
-    
+
     static Type *Int;
     static Type *Float;
     static Type *Char;
     static Type *Bool;
-    
+
     static bool numric(Type *p);
+
     static Type *max(Type *p1, Type *p2);
 };
 
-class Array : public Type {
+class Array : public Type
+{
 public:
-    Type    *of;
-    int     size;
+    Type *of;
+    int size;
+
     Array(int sz, Type *p);
-    const char *toString();
+
+    const char *toString() override;
 };
 
-class Num : public Token {
+class Num : public Token
+{
 public:
     int value;
 public:
-    Num(int v);
-    const char *toString();
+    explicit Num(int v);
+
+    const char *toString() override;
 };
 
-class Real : public Token {
+class Real : public Token
+{
 public:
     float value;
 public:
-    Real(float v);
-    const char *toString();
+    explicit Real(float v);
+
+    const char *toString() override;
 };
